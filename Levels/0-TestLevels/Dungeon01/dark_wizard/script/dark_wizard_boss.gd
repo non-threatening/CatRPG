38504b59b9a510c1995c3c7c9701b4e8ebe63193
non-threatening <imaginary.theremin@gmail.com>
2025-05-11 18:57:45 +0200

    enemy pathfinder

--- Levels/0-TestLevels/Dungeon01/dark_wizard/script/dark_wizard_boss.gd
+++ Levels/0-TestLevels/Dungeon01/dark_wizard/script/dark_wizard_boss.gd
@@ -60,7 +60,7 @@ func _ready() -> void:
 	
 	
 	
-func _process(delta: float) -> void:
+func _process( _delta: float ) -> void:
 	hand_01_up.position == hand_01.position
 	hand_01_up.frame = hand_01.frame + 4
 	hand_02_up.position == hand_02.position
