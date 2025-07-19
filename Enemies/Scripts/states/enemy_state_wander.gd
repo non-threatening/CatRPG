ea71d181e189fd42613c1329b5a4b51f628f1dab
non-threatening <imaginary.theremin@gmail.com>
2025-07-19 20:54:55 +0200

    tweeks

--- Enemies/Scripts/states/enemy_state_wander.gd
+++ Enemies/Scripts/states/enemy_state_wander.gd
@@ -23,8 +23,8 @@ func enter() -> void:
 	_timer = randi_range( state_cycles_min, state_cycles_max ) * state_animation_duration
 	var rand = randi_range( 0, 3 )
 	_direction = enemy.DIR_4[ rand ]
-	enemy.velocity = _direction * wander_speed
 	enemy.set_direction( _direction )
+	enemy.velocity = _direction * wander_speed
 	enemy.update_animation( anim_name )
 	pass
 	
