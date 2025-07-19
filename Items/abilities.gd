133aeeba4db4a753c98313baa2900b509e9229b6
non-threatening <imaginary.theremin@gmail.com>
2025-07-19 21:01:03 +0200

    Bird Friend

--- Items/abilities.gd
+++ Items/abilities.gd
@@ -1,14 +1,16 @@
 class_name PlayerAbilities extends Node
 
 const BOOMERANG = preload("res://Player/boomerang.tscn")
+const BIRD = preload("res://Player/bird_friend.tscn")
 
 var abilities : Array[ String ] = [
-	"BOOMERANG", "GRAPPLE", "BOW", "BOMB"
+	"BOOMERANG", "BIRD", "BOW", "BOMB"
 	]
 
 var selected_ability = 0
 var player : Player
 var boomerang_instance : Boomerang = null
+var bird_instance : BirdFriend = null
 
 
 
@@ -25,11 +27,12 @@ func _unhandled_input(event: InputEvent) -> void:
 			0:
 				boomerang_ability()
 			1:
-				print("Grapple")
+				bird_ability()
 			2:
 				print("Bow")
 			3:
 				print("Bomb")
+				
 	elif event.is_action_pressed("switch_ability"):
 		toggle_ability()
 	pass
@@ -38,6 +41,25 @@ func toggle_ability() -> void:
 	selected_ability = wrapi( selected_ability + 1, 0, 4 )
 	PlayerHud.update_ability_ui( selected_ability )
 	pass
+	
+func bird_ability() -> void:
+	if bird_instance != null: # Do we have a boomerang? Limits number of boomerangs to 1
+		return
+	
+	var _b = BIRD.instantiate() as BirdFriend
+	player.add_sibling( _b ) # make it a sibling of the player node so its at the same Z
+	_b.global_position = player.global_position
+	
+	var throw_direction = player.direction
+	if throw_direction == Vector2.ZERO:
+		throw_direction = player.cardinal_direction
+		
+	_b.throw( throw_direction )
+	bird_instance = _b
+	pass
+
+
+
 
 func boomerang_ability() -> void:
 	if boomerang_instance != null: # Do we have a boomerang? Limits number of boomerangs to 1
---
+++ Player/Scripts/abilities/bird_friend.gd
@@ -0,0 +1,84 @@
+class_name BirdFriend extends Node2D
+
+enum State { INACTIVE, THROW, RETURN }
+
+const DIR_4 = [ Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP ]
+
+var player : Player
+var direction : Vector2
+var flight_direction : Vector2
+var speed : float = 0
+var state
+
+@export var acceleration : float = 500.0
+@export var max_speed : float = 666.0
+@export var catch_audio : AudioStream
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+
+func _ready() -> void:
+	visible = false
+	state = State.INACTIVE
+	player = PlayerManager.player
+	_set_random_color()
+	
+
+func _set_random_color() -> void:
+	var new_color = Color.from_hsv(randf_range( 0, 1 ), 1.0, 1.0, 1.0)
+	sprite.material.set_shader_parameter( "new_color", new_color )
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	if state == State.THROW:
+		speed -= acceleration * delta
+		position += direction * speed * delta
+		if speed <= 0:
+			state = State.RETURN
+			update_animation()
+		pass
+	elif state == State.RETURN:
+		direction = global_position.direction_to( player.global_position )
+		speed += acceleration * delta
+		position += direction * speed * delta
+		if global_position.distance_to( player.global_position ) <= 10: # Remove it when it's close to the player, it's caught
+			PlayerManager.play_audio( catch_audio )
+			queue_free() 
+		pass
+	
+	var speed_ratio = speed / max_speed
+	audio.pitch_scale = speed_ratio * 0.85 + 1.0
+	animation_player.speed_scale = 1 + ( speed_ratio * 0.25 )
+	pass
+	
+	
+func throw( throw_direction : Vector2 ) -> void:
+	#print(throw_direction)
+	direction = throw_direction
+	speed = max_speed
+	state = State.THROW
+	update_animation()
+	PlayerManager.play_audio( catch_audio ) ## Plays in PlayerManager node
+	visible = true
+	pass
+
+
+func update_animation() -> void:
+	await get_tree().process_frame
+	await get_tree().process_frame # wait an extra frame to process Return state and change direction
+	var direction_id : int = int( round( ( direction * 0.1 ).angle() / TAU * DIR_4.size() ) )
+	flight_direction = DIR_4[ direction_id ]
+	sprite.scale.x = -1 if flight_direction == Vector2.LEFT else 1
+	
+	animation_player.play( "fly_" + anim_direction() )
+	
+
+func anim_direction() -> String:
+	if flight_direction == Vector2.DOWN:
+		return "down"
+	elif flight_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
