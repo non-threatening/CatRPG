e1ca18ff8a25bab8fae142d34a861b668ca97684
non-threatening <imaginary.theremin@gmail.com>
2025-10-12 01:25:43 +0200

    Controller_icons gosotsteam

---
+++ 00_Globals/Steam/steam_controller.gd
@@ -0,0 +1,30 @@
+extends Node
+
+
+func _init() -> void:
+	Steam.steamInitEx( 4097910, true )
+	print( "response: ", Steam.get_steam_init_result() )
+	
+	var username : String = Steam.getPersonaName()
+	print( "username: ", username )
+	
+	#var steam_id : int = Steam.getSteamID()
+	#print( "Steam ID", steam_id )
+	
+	
+	#var app_installed_depots: Array = Steam.getInstalledDepots( steam_id )
+	#var app_languages: String = Steam.getAvailableGameLanguages()
+	#var app_owner: int = Steam.getAppOwner()
+	#print(app_owner)
+	#var build_id: int = Steam.getAppBuildId()
+	#var game_language: String = Steam.getCurrentGameLanguage()
+	##var install_dir : Dictionary = Steam.getAppInstallDir( steam_id )
+	#var is_on_steam_deck: bool = Steam.isSteamRunningOnSteamDeck()
+	#print( "is steamdeck: ", is_on_steam_deck )
+	#var is_on_vr: bool = Steam.isSteamRunningInVR()
+	#var is_online: bool = Steam.loggedOn()
+	#var is_owned: bool = Steam.isSubscribed()
+	#var launch_command_line: String = Steam.getLaunchCommandLine()
+	#var ui_language: String = Steam.getSteamUILanguage()
+	#
+	#print( ui_language, " ", game_language )
--- Dialogue/interact_indicator.gd
+++ Dialogue/interact_indicator.gd
@@ -1,18 +1,18 @@
 @tool
 class_name InteractIndicator extends Area2D
 
-@export var x : int = 0 : set = _set_x
+@export var x : int = -32 : set = _set_x
 @export var y : int = -128 : set = _set_y
 
 @onready var indicator_2d: InteractIndicator = $"."
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var texture_rect: TextureRect = $TextureRect
 
 
 func _ready() -> void:
 	indicator_2d.body_entered.connect( _on_body_entered )
 	indicator_2d.body_exited.connect( _on_body_exited )
-	sprite_2d.position.x = x
-	sprite_2d.position.y = y
+	texture_rect.position.x = x
+	texture_rect.position.y = y
 	if Engine.is_editor_hint():
 		indicator_2d.show()
 		indicator_2d.modulate.a = 0.5
@@ -24,22 +24,22 @@ func _ready() -> void:
 func _on_body_entered( _a ) -> void:
 	indicator_2d.show()
 	var tween = get_tree().create_tween()
-	tween.tween_property(indicator_2d, "modulate", Color(1.0, 0.565, 0.537, 1.0), 0.35)
+	tween.tween_property(indicator_2d, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.35)
 	
 	
 func _on_body_exited( _a ) -> void:
 	var tween = get_tree().create_tween()
-	tween.tween_property(indicator_2d, "modulate", Color( 1.0, 0.565, 0.537, 0.0 ), 0.35)
+	tween.tween_property(indicator_2d, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.35)
 	await get_tree().create_timer( 0.35 ).timeout
 	indicator_2d.hide()
 
-func _set_x( _a ) -> void:
+func _set_x( _a : = -32 ) -> void:
 	x = _a
-	if sprite_2d:
-		sprite_2d.position.x = _a
+	if texture_rect:
+		texture_rect.position.x = _a
 
 
 func _set_y( _a : int = -128 ) -> void:
 	y = _a
-	if sprite_2d:
-		sprite_2d.position.y = _a
+	if texture_rect:
+		texture_rect.position.y = _a
